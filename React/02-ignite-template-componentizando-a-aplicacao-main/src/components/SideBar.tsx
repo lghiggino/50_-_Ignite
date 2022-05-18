e209abf99a32b26fda573fc0e980a82b2c5e5a63
lghiggino <lghiggino@gmail.com>
2022-05-18 10:11:45 -0300

    SideBar component with props === any

--- React/02-ignite-template-componentizando-a-aplicacao-main/src/components/SideBar.tsx
+++ React/02-ignite-template-componentizando-a-aplicacao-main/src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button"
+
+interface SideBarProps {
+  genres: any
+  selectedGenreId: any
+  setSelectedGenreId: any
+}
+
+export function SideBar({genres, selectedGenreId, setSelectedGenreId}: SideBarProps) {
+
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map((genre:any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
