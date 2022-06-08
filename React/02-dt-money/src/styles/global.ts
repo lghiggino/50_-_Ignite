e4dcaddf0ebec6089611376857f55c27dbffc707
lghiggino <lghiggino@gmail.com>
2022-06-08 10:36:38 -0300

    fim da estilização global

--- React/02-dt-money/src/styles/global.ts
+++ React/02-dt-money/src/styles/global.ts
@@ -2,19 +2,56 @@ import { css } from "@emotion/react";
 
 export const GlobalStyles = css`
     :root {
-        --background: #F0F2F5;
+        --red: #E52E4D;
+        --green: #33cc95;
+        --blue: #5429cc;
+
+
+        --blue-light: #6933ff;
+
+        --text-title: #363f5f;
+        --text-body: #969c83;
+
+        --background: #f0f2f5;
+        --shape: #ffffff;
     }
 
-    
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
 
+    html {
+        @media (max-width: 1080px){
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px){
+            font-size: 87.5%;
+        }
+    }
+
     body {
-        background-color: var(--background);
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `
-
-//continuar aqui: https://app.rocketseat.com.br/node/chapter-ii-1/group/estrutura-da-aplicacao-1/lesson/criando-estilos-globais
