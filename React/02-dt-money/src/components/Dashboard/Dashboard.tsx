a5cc795af3c8d70ff55c3e729abb5c1aaa7adab3
lghiggino <lghiggino@gmail.com>
2022-06-11 09:03:55 -0300

    TransactionsTable is styled

--- React/02-dt-money/src/components/Dashboard/Dashboard.tsx
+++ React/02-dt-money/src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { Container } from "../Dashboard/Dashboard.styles";
 import { Summary } from "../Summary/Summary";
+import { TransactionsTable } from "../TransactionsTable/TransactionsTable";
 
 export function Dashboard(){
     return (
         <Container>
             <Summary />
+            <TransactionsTable />
         </Container>
     )
 }
--- React/02-dt-money/src/components/TransactionsTable/TransactionsTable.tsx
+++ React/02-dt-money/src/components/TransactionsTable/TransactionsTable.tsx
@@ -3,7 +3,31 @@ import { Container } from "./TransactionsTable.styles";
 export function TransactionsTable(){
     return(
         <Container>
-            table
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td className="deposit">R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/06/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$ 2.000</td>
+                        <td>Casa</td>
+                        <td>17/06/2021</td>
+                    </tr>
+                </tbody>
+            </table>
         </Container>
     )
 }
