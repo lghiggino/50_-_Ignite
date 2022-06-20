e3a1d8a53c58e9ec80cced1300a9585f4066d227
lghiggino <lghiggino@gmail.com>
2022-06-20 07:35:53 -0300

    remoção do teste de Suspense para componente proprio

--- React/02-dt-money/src/App.tsx
+++ React/02-dt-money/src/App.tsx
@@ -4,7 +4,6 @@ import { Dashboard } from "./components/Dashboard/Dashboard";
 import { Header } from "./components/Header/Header";
 import { LoginHeader } from "./components/LoginHeader/LoginHeader";
 import { GlobalStyles } from "./styles/global";
-import Modal from 'react-modal'
 import { NewTransactionModal } from "./components/NewTransactionModal/NewTransactionModal";
 import { LoginModal } from "./components/LoginModal/LoginModal";
 import { RegisterModal } from "./components/RegisterModal/RegisterModal";
@@ -15,13 +14,6 @@ export function App() {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
 
-  const resource = fetchProfileData();
-
-  function ProfileDetails() {
-    // Try to read user info, although it might not have loaded yet
-    const user = resource.user.read();
-    return <h1>{user.name}</h1>;
-  }
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
@@ -52,14 +44,10 @@ export function App() {
     <div className="App">
       <Global styles={GlobalStyles} />
 
-      <Suspense
-        fallback={<h1>loading....</h1>}
-      >
-        <ProfileDetails />
-      </Suspense>
-
       {!user &&
-        <>
+        <Suspense
+          fallback={<h1>loading....</h1>}
+        >
           <LoginHeader
             setUser={setUser}
             onOpenLoginModal={handleOpenLoginModal}
@@ -68,15 +56,17 @@ export function App() {
           <div>
             <h1 style={{ textAlign: 'center' }}>Welcome to DtMoney</h1>
           </div>
-        </>
+        </Suspense>
 
       }
 
       {user &&
-        <>
+        <Suspense
+          fallback={<h1>loading....</h1>}
+        >
           <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
           <Dashboard />
-        </>
+        </Suspense>
       }
 
       <NewTransactionModal
@@ -99,72 +89,4 @@ export function App() {
 }
 
 
-
-export function fetchProfileData() {
-  let userPromise = fetchUser();
-  let postsPromise = fetchPosts();
-  return {
-    user: wrapPromise(userPromise),
-    posts: wrapPromise(postsPromise)
-  };
-}
-
-function wrapPromise(promise: any) {
-  let status = "pending";
-  let result: any;
-  let suspender = promise.then(
-    (r: any) => {
-      status = "success";
-      result = r;
-    },
-    (e: any) => {
-      status = "error";
-      result = e;
-    }
-  );
-  return {
-    read() {
-      if (status === "pending") {
-        throw suspender;
-      } else if (status === "error") {
-        throw result;
-      } else if (status === "success") {
-        return result;
-      }
-    }
-  };
-}
-
-function fetchPosts() {
-  console.log("fetch posts...");
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("fetched posts");
-      resolve([
-        {
-          id: 0,
-          text: "I get by with a little help from my friends"
-        },
-        {
-          id: 1,
-          text: "I'd like to be under the sea in an octupus's garden"
-        },
-        {
-          id: 2,
-          text: "You got that sand all over your feet"
-        }
-      ]);
-    }, 1100);
-  });
-}
-
-function fetchUser() {
-  return fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(resposta => {
-    return resposta.json()
-  })
-    .then(json => {
-      console.log(json);
-      return json;
-    })
-}
 
