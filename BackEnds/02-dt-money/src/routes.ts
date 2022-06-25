22bb42afb894ba90ec78d93bab874de17e92d7fc
lghiggino <lghiggino@gmail.com>
2022-06-25 11:59:45 -0300

    getTolkenFrom request

--- BackEnds/02-dt-money/src/routes.ts
+++ BackEnds/02-dt-money/src/routes.ts
@@ -1,6 +1,8 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { TransactionService } from "./service/TransactionService";
 import { UserService } from "./service/UserService";
+import jwt from "jsonwebtoken";
+import { ApiError } from "./errors/ApiError";
 
 export const routes = Router()
 
@@ -12,27 +14,51 @@ routes.get('/', (req, res) => {
     </div>`)
 })
 
-routes.post('/transaction', async (req, res) => {
-    const { title, amount, type, category, userId, User } = req.body
+const getTokenFrom = (request: Request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
+routes.post('/transaction', async (request, response) => {
+    const { title, amount, type, category, userId, User } = request.body
+
+    const token = getTokenFrom(request)
+    if (!token) {
+        throw new ApiError('Decode token error', 400, 'Token invalid or expired')
+    }
+
+    const decodedToken: any = jwt.verify(token, process.env.SECRET as string)
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await UserService.findById(decodedToken.id)
+
+    if (user === "invalid user") {
+        return  "invalid user"
+    }
 
     const transaction = await TransactionService.create({
         title,
         amount,
         type,
         category,
-        userId,
-        User
+        userId: user.id,
+        User: user
     })
 
-    return res.status(201).json({ data: transaction })
+    return response.status(201).json({ data: transaction })
 })
 
-routes.get('/transaction/:userId', async (req, res) => {
-    const { userId } = req.params
+routes.get('/transaction/:userId', async (request, response) => {
+    const { userId } = request.params
 
     const transactionList = await TransactionService.getByUserId(userId)
 
-    return res.status(201).json({ data: transactionList })
+    return response.status(201).json({ data: transactionList })
 })
 
 routes.post('/createuser', async (req, res) => {
@@ -49,14 +75,14 @@ routes.post('/createuser', async (req, res) => {
     return res.status(201).json({ data: newUser })
 })
 
-routes.post('/login', async (req, res) => {
-    const { email, password } = req.body
+routes.post('/login', async (request, response) => {
+    const { email, password } = request.body
 
     const user = await UserService.login({ email, password })
-    
-    if (user === "invalid email or password"){
-        return res.status(400).json({error: "invalid email or password"})
+
+    if (user === "invalid email or password") {
+        return response.status(400).json({ error: "invalid email or password" })
     }
 
-    return res.json(user)
+    return response.json(user)
 })
