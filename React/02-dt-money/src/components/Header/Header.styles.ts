79919fbe191eb1d57f72cf3768c4c374331ff3f0
lghiggino <lghiggino@gmail.com>
2022-07-03 12:35:24 -0300

    Header style for double buttons while logged in

--- React/02-dt-money/src/components/Header/Header.styles.ts
+++ React/02-dt-money/src/components/Header/Header.styles.ts
@@ -12,22 +12,6 @@ export const Content = styled.div`
     align-items: center;
     justify-content: space-between;
 
-    /* button{
-        font-size: 1rem;
-        color: #fff;
-        background-color: var(--blue-light);
-        border: 0;
-        padding: 0 2rem;
-        border-radius: 0.25rem;
-        height: 3rem;
-
-        transition: filter 0.2s;
-
-        &:hover{
-            filter: brightness(0.9);
-        }
-    } */
-
     .button-container{
         display: flex;
         flex-direction: column;
